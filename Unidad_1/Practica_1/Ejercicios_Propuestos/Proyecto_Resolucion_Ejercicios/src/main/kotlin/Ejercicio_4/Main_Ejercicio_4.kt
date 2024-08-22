bfa59687ef10addb324959eff86ae8b8c8231759
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-22 10:02:47 -0500

    Creando Funcion leerNumero, para el manejo de Excepciones

--- Unidad_1/Practica_1/Ejercicios_Propuestos/Proyecto_Resolucion_Ejercicios/src/main/kotlin/Ejercicio_4/Main_Ejercicio_4.kt
+++ Unidad_1/Practica_1/Ejercicios_Propuestos/Proyecto_Resolucion_Ejercicios/src/main/kotlin/Ejercicio_4/Main_Ejercicio_4.kt
@@ -2,13 +2,50 @@ package Ejercicio_4
 
 import kotlin.random.Random
 
+fun leerNumero(): Int{
+    //Se crea una variable que acepta nulos, por el caso de la conversión
+    var numeroFinal: Int?
+
+    while(true){
+        //Leemos la puntuacion de manera explicita
+        print("Ingrese su Puntuacion: ")
+        var numero = readln()
+
+        // Generacion de try-catch en caso de una mala conversion
+        try{
+            //Realizando la conversion a entero
+            numeroFinal = numero.toInt()
+
+            //En caso de ser null, no se ha ingrasado un valor
+            if (numeroFinal != null){
+                //Preguntando si valor está de 0 a 30
+                if (numeroFinal in 1..30){
+                    return numeroFinal
+                }
+                else{
+                    //En caso de que el numero esté por fuera del limite
+                    println("--Debe ingresar una puntuacion desde 1 a 30--")
+                }
+            }
+            else{
+                //En caso de no haber ingresado un numero
+                println("--Debe Igresar un numero--")
+            }
+        }
+        catch(e: NumberFormatException){
+            //Caso de no haber ingresado un valor numerico entero
+            println("--Debe Ingresar un numero valido--")
+        }
+    }
+}
+
 fun main(){
     var cantidadIntentos = 5
     val numeroAleatorio = Random.nextInt(1,31)
     while (cantidadIntentos != 0){
         print("\rIntentos: $cantidadIntentos\n")
         print("\rAdiva un numero: ")
-        var numero = readln().toInt()
+        var numero = leerNumero()
         var pista: String? = ""
         if (numero > numeroAleatorio){
             pista = "El numero es menor"
