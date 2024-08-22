2636eaee11ec4fc0264ce5c63f3d5ed35ef9c0a2
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-21 22:47:41 -0500

    Creando la funcion: obtenerOpcionCalculadora, que registra el menú de opciones.

--- Unidad_1/Practica_1/Ejercicios_Propuestos/Proyecto_Resolucion_Ejercicios/src/main/kotlin/Ejercicio_3/Main_Ejercicio_3.kt
+++ Unidad_1/Practica_1/Ejercicios_Propuestos/Proyecto_Resolucion_Ejercicios/src/main/kotlin/Ejercicio_3/Main_Ejercicio_3.kt
@@ -1,15 +1,44 @@
 package Ejercicio_3
 
-fun main(){
-    println("==== Menu ====")
-    println("1. Suma")
-    println("2. Resta")
-    println("3. Multiplicacion")
-    println("4. Division")
-    println("5. Salir")
+fun obtenerOpcionCalculadora(): Int{
+    var opcionCalculadora: Int?
+    while (true) {
+        println("==== Menu ====")
+        println("1. Suma")
+        println("2. Resta")
+        println("3. Multiplicacion")
+        println("4. Division")
+        println("5. Salir")
 
-    print("\nSu opcion: ")
-    var opcion = readln().toInt()
+        print("\nSu opcion: ")
+        var opcion = readln()
+
+        try{
+            opcionCalculadora = opcion.toInt()
+            if (opcionCalculadora != null) {
+                if (opcionCalculadora in 1..5){
+                    return opcionCalculadora
+                }
+                else{
+                    println("Ingrese una opcion pertinente")
+                }
+            }
+            else{
+                println("Debe ingresar una opcion")
+            }
+        }
+        catch (e: NumberFormatException){
+            println("Ingrese un valor entero")
+        }
+    }
+}
+
+fun main(){
+    var opcion = obtenerOpcionCalculadora()
+
+    if (opcion == 5){
+        return
+    }
 
     var valor1 = readln().toDouble()
     var valor2 = readln().toDouble()
