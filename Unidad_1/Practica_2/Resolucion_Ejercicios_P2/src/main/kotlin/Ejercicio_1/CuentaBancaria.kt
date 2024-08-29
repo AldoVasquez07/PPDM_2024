1c79afa091380b0bc3dce5b5c53d7845bdc9b7ee
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-28 23:22:21 -0500

    Creando Main para la comprobación de la clase

--- Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/CuentaBancaria.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/CuentaBancaria.kt
@@ -3,13 +3,16 @@ package Ejercicio_1
 class CuentaBancaria {
     var numero: String = ""
     var contrasena: String = ""
-    var saldo: Double = 0.0
-    var limite: Double = 0.0
+    var saldo: Double = 1000.0
+    var limite: Double = 500.0
+    var montoRetirado: Double = 0.0
     var fechaCreacion: String = "dd/mm/aaaa"
 
     fun retiro(cantidad: Double){
-        if (cantidad <= limite) {
+        if (montoRetirado + cantidad <= limite) {
             saldo -= cantidad
+            montoRetirado += cantidad
+            println("Monto Retirado: $montoRetirado")
         }
         else{
             println("Ya excedió el límite de su tarjeta")
--- Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/Main.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/Main.kt
@@ -1,10 +1,8 @@
 package Ejercicio_1
 
-fun main(args: Array<String>) {
+fun main() {
     val cuenta = CuentaBancaria()
-    println(cuenta.limite)
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    while(true){
+        cuenta.retiro(readln().toDouble())
+    }
 }
