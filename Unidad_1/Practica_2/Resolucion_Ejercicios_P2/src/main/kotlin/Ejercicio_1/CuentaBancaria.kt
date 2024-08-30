f60826bb75b54fbca26adf1020559e2cfa0ea83b
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-30 13:09:16 -0500

    Añadiendo Restricciones

--- Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/CuentaBancaria.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/CuentaBancaria.kt
@@ -9,42 +9,20 @@ class CuentaBancaria {
     var limite: Double = 0.0
     var montoRetirado: Double = 0.0
     var fechaCreacion: String = ""
-    var estado: Boolean = false
+    var estado: Boolean = true
 
-    fun numeroSet(numero: String){
-        this.numero = numero
-    }
-
-    fun numeroGet(): String{
-        return this.numero
-    }
-
-    fun contrasenaSet(contrasena: String){
-        this.contrasena = contrasena
-    }
-
-    fun contrasenaGet(): String{
-        return this.contrasena
-    }
-
-    fun saldoSet(saldo: Double){
-        this.saldo = saldo
-    }
-
-    fun saldoGet(): Double{
-        return this.saldo
-    }
 
     fun crearCuentaBancaria(){
-        if (!this.estado) {
+        if (this.estado) {
             var utils = Utils()
-            println("Creación de Cuenta: ")
+            println("**** Creación de Cuenta ****")
             this.numero = utils.determinarLongitudCadena("Numero de cuenta: ", 10)
             print("Contraseña: ")
             this.contrasena = readln()
             this.saldo = utils.conversionNumero("Saldo: ")
             this.limite = utils.conversionNumero("Limite: ")
             this.fechaCreacion = LocalDateTime.now().toString()
+            this.estado = false
         }
         else{
             println("-- Usted ya tiene una cuenta --")
@@ -52,18 +30,27 @@ class CuentaBancaria {
     }
 
     fun retiro(cantidad: Double){
-        if (montoRetirado + cantidad <= limite) {
-            saldo -= cantidad
-            montoRetirado += cantidad
-            println("... Monto Retirado: $montoRetirado ...")
+        if (!this.estado) {
+            if (montoRetirado + cantidad <= limite) {
+                saldo -= cantidad
+                montoRetirado += cantidad
+                println("... Monto Retirado: $cantidad ...\n")
+            } else {
+                println("--Ya excedió el límite de su tarjeta--")
+            }
         }
         else{
-            println("--Ya excedió el límite de su tarjeta--")
+            println("-- Necesita Crear una Cuenta --")
         }
     }
 
     fun recargarSaldo(cantidad: Double){
-        this.saldo += cantidad
-        println("Monto: $cantidad Ingresado con exito")
+        if(!this.estado) {
+            this.saldo += cantidad
+            println("... Monto: $cantidad Ingresado con exito ...\n")
+        }
+        else{
+            println("-- Necesita Crear una Cuenta --")
+        }
     }
 }
--- Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/Main.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio_1/Main.kt
@@ -1,57 +1,67 @@
 package Ejercicio_1
 
 fun leerOpcion(): Int{
+    print("\nSu opcion: ")
     // Generando Lista con opciones válidas
     val opcionesValidas = listOf("1", "2", "3", "4")
     while (true) {
         val opcion = readln()
         // Consultando si la opcion ingresada es válida
         if (opcion in opcionesValidas) {
+            // Retornando valor
             return opcion.toInt()
         } else {
+            // Imprimiendo caso de no ingresar una opcion valida
             println("-- Debe ingresar una Opcion Válida--")
         }
     }
 }
 
-fun mostrarMenu(): Int{
+fun mostrarMenu() {
     // Declarando separadores estaticos
-    val separador1 =  "//////////////////////////////////////////////////"
-    val separador2 = "*****************************"
+    val separador = "*****************************"
     // Imprimiendo Menú
-    println(separador1)
-    println(separador2)
+    println(separador)
     println("*** ¡Bienvenido al Banco! ***")
-    println("$separador2\n")
+    println("$separador\n")
     println("<< Ingrese una opción >>")
     println("1. Crear Cuenta")
     println("2. Retirar")
     println("3. Recargar")
     println("4. Salir")
-
-    return leerOpcion()
 }
 
 fun identificarOpcion(cuenta: CuentaBancaria, utils: Utils, opcion: Int){
 
     when (opcion){
+        // Creando cuenta
         1 -> cuenta.crearCuentaBancaria()
+        // Retirando saldo Cuenta
         2 -> cuenta.retiro(utils.conversionNumero("-> Ingrese la cantidad a Retirar: "))
+        // Recargando Cuenta
         3 -> cuenta.recargarSaldo(utils.conversionNumero("-> Ingrese el saldo a Recargar: "))
     }
 }
 
 fun main() {
+    // Instanciando Clases Vitales
     var cuenta = CuentaBancaria()
     var utils =  Utils()
+    // Declarando separador
+    val separador =  "//////////////////////////////////////////////////"
 
+    // Menu Basico
     while (true) {
-        val opcion = mostrarMenu()
+        println(separador)
+        mostrarMenu()
+        var opcion = leerOpcion()
 
+        // Retornando en caso de salir del menu
         if (opcion == 4) {
             return
         }
-
+        // Ejecutando acción establecida
         identificarOpcion(cuenta, utils, opcion)
+        println(separador)
     }
 }
