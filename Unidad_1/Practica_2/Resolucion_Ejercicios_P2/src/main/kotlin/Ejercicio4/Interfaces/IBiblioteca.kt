6f7d7fc5c63b4985a4f8a4b4a397ff01b667c5a5
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-30 23:35:48 -0500

    Creando Interface IBiblioteca, y la clase Biblioteca, la cual implementa la Interface

---
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio4/Interfaces/IBiblioteca.kt
@@ -0,0 +1,10 @@
+package Ejercicio4.Interfaces
+
+interface IBiblioteca{
+    fun registrarMaterial()
+    fun registrarUsuario()
+    fun prestamo()
+    fun devolucion()
+    fun mostrarMaterialesDisponibles()
+    fun mostrarMaterialesReservados()
+}
---
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio4/Modelo/Biblioteca.kt
@@ -0,0 +1,7 @@
+package Ejercicio4.Modelo
+
+import Ejercicio4.Interfaces.IBiblioteca
+
+class Biblioteca: IBiblioteca {
+
+}
