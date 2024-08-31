5717d1aab3fdc47e82b13641e478e9c242736e38
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-30 20:49:29 -0500

    Generando Funciones para el menú y para el submenú

--- Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio3/Controlador/MainEjercicio3.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio3/Controlador/MainEjercicio3.kt
@@ -1,2 +1,22 @@
 package Ejercicio3.Controlador
 
+fun mostrarMenu(){
+    println("¡Bienvenido!")
+    println("Elija una Opcion")
+    println("1. Circulo")
+    println("2. Cuadrado")
+    println("3. Rectangulo")
+    println("4. Salir")
+}
+
+fun mostrarSubMenu(figura: String){
+    println("... Figura: $figura ...")
+    println("¿Que desea calcular?")
+    println("1. Area")
+    println("2. Perimetro")
+    println("3. Volver")
+}
+
+fun main(){
+    return
+}
