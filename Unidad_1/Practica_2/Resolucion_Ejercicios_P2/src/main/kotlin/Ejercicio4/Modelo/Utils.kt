19f6f0a70db55cfa67fb5fc1e60804b9b37c5029
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-08-31 00:03:56 -0500

    Reutilizando la clase Utils

---
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_P2/src/main/kotlin/Ejercicio4/Modelo/Utils.kt
@@ -0,0 +1,64 @@
+package Ejercicio4.Modelo
+
+class Utils {
+
+    fun conversionEntero(mensaje: String, limiteInferior: Int, limiteSuperior: Int): Int{
+        while (true) {
+            // Imprimiendo mensaje pasado por parametro
+            print(mensaje)
+            // Leyendo opcion
+            var num = readln()
+            try {
+                // Realizando conversión a entero
+                var numero = num.toInt()
+                if (numero != null){
+                    if (numero >= limiteInferior && numero <= limiteSuperior) {
+                        // Retornando si el numero está dentro de las opciones
+                        return numero
+                    }
+                    else{
+                        // Caso de no ingresar una opcion dentro del limite
+                        println("-- Ingrese una opcion valida --")
+                    }
+                }
+                else{
+                    // En caso de que al convertir sea nulo
+                    println("-- Debe ingresar una opcion --")
+                }
+            } catch (e: NumberFormatException) {
+                // Caso de error en la conversion a entero
+                println("-- Debe ingresar un numero --")
+            }
+        }
+    }
+
+    fun conversionDecimal(mensaje: String, limiteInferior: Double, limiteSuperior: Double): Double{
+        while (true) {
+            // Imprimiendo mensaje pasado por parametro
+            print(mensaje)
+            // Leyendo opcion
+            var num = readln()
+            try {
+                // Realizando conversión a entero
+                var numero = num.toDouble()
+                if (numero != null){
+                    if (numero >= limiteInferior && numero <= limiteSuperior) {
+                        // Retornando si el numero está dentro de las opciones
+                        return numero
+                    }
+                    else{
+                        // Caso de no ingresar una opcion dentro del limite
+                        println("-- Ingrese una opcion valida --")
+                    }
+                }
+                else{
+                    // En caso de que al convertir sea nulo
+                    println("-- Debe ingresar una opcion --")
+                }
+            } catch (e: NumberFormatException) {
+                // Caso de error en la conversion a entero
+                println("-- Debe ingresar un numero --")
+            }
+        }
+    }
+}
