ce9dda373e7a7d934f21b8086e5817e85507a2a8
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-15 17:12:12 -0500

    Añadiendo la lógica de spinner al activity principal

--- Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/MainActivity.kt
+++ Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.ppdm.app_ejercicio_1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
