7bd26ff6b1bbb2b3e26ab90ead3b06210416437d
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-15 18:00:49 -0500

    Agregando Imagenes, XML y clase Activity, para la funcionalidad para la lógica

--- Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/ImageActivity.kt
+++ Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/ImageActivity.kt
@@ -1,8 +1,38 @@
 package com.ppdm.app_ejercicio_1
 
-
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ImageActivity : AppCompatActivity() {
 
+    private lateinit var imageView: ImageView
+    private lateinit var backButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+
+        imageView = findViewById(R.id.imageView)
+        backButton = findViewById(R.id.backButton)
+
+
+        val imageIndex = intent.getIntExtra("image_index", 0)
+
+        // Lista de Imagenes
+        val images = arrayOf(
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3
+        )
+
+        // Renderizar la Imagen deseada
+        imageView.setImageResource(images[imageIndex])
+
+        // Retornar a la Funcion Anterior
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
 }
