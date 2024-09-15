7bfc56f19d8969fd02bdc58caf23b7b43baa4ea9
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-15 17:58:46 -0500

    Subiendo el XML para las imagenes y una clase activity para las imagenes

---
+++ Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/ImageActivity.kt
@@ -0,0 +1,8 @@
+package com.ppdm.app_ejercicio_1
+
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageActivity : AppCompatActivity() {
+
+}
--- Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/MainActivity.kt
+++ Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ppdm.app_ejercicio_1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageSpinner: Spinner
+    private lateinit var nextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageSpinner = findViewById(R.id.imageSpinner)
+        nextButton = findViewById(R.id.nextButton)
+
+        val imageNames = arrayOf("Image 1", "Image 2", "Image 3")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, imageNames)
+        imageSpinner.adapter = adapter
     }
 }
