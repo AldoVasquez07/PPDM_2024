6cad6f356b3dfc9ffb814d1aa3a44372f241f481
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-16 12:17:00 -0500

    Reutilizando la clase MainActivity del Ejercicio 1

--- Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_2/app/src/main/java/com/ppdm/app_ejercicio_2/MainActivity.kt
+++ Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_2/app/src/main/java/com/ppdm/app_ejercicio_2/MainActivity.kt
@@ -1,15 +1,44 @@
 package com.ppdm.app_ejercicio_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // Declarando Atributos
+    private lateinit var imageSpinner: Spinner
+    private lateinit var nextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Realizando búsqueda de los componentes XML
+        imageSpinner = findViewById(R.id.imageSpinner)
+        nextButton = findViewById(R.id.nextButton)
+
+        // Pasando la Información al Spinner del Array de Opciones
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, Utils.imageNames)
+        imageSpinner.adapter = adapter
+
+        // Mantener la selección del Spinner al rotar
+        if (savedInstanceState != null) {
+            imageSpinner.setSelection(savedInstanceState.getInt(Utils.spinnerPosition))
+        }
+
+        // LLamando a la Actividad: ImageActivity, para mostrar la imagen
+        nextButton.setOnClickListener {
+            val selectedImage = imageSpinner.selectedItemPosition
+            //val intent = Intent(this, ImageActivity::class.java)
+            intent.putExtra(Utils.imageIndex, selectedImage)
+            startActivity(intent)
+        }
 
     }
 }
