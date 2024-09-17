2d0f45ea184326f5761bf2da84cc88d96dcb0678
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-16 21:20:33 -0500

    Agregando Imagenes de las bandas según la canción elegida.

--- Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_2/app/src/main/java/com/ppdm/app_ejercicio_2/PlayBackActivity.kt
+++ Unidad_2/Practica_3/Ejercicios_Propuestos/App_Ejercicio_2/app/src/main/java/com/ppdm/app_ejercicio_2/PlayBackActivity.kt
@@ -27,6 +27,7 @@ class PlayBackActivity : AppCompatActivity() {
         pauseButton = findViewById(R.id.pauseButton)
         stopButton = findViewById(R.id.stopButton)
 
+        // Obtener el índice del audio seleccionado
         currentAudioIndex = intent.getIntExtra("audio_index", 0)
         val audioFiles = arrayOf(
             R.raw.a_king_of_magic,
@@ -36,8 +37,12 @@ class PlayBackActivity : AppCompatActivity() {
             R.raw.someone_to_spend_time_with
         )
 
+        // Establecer el título del audio
         audioTitle.text = Utils.audioNames[currentAudioIndex]
+
+        // Establecer el archivo de audio y el ícono
         mediaPlayer = MediaPlayer.create(this, audioFiles[currentAudioIndex])
+        setAudioImage(currentAudioIndex)
 
         playButton.setOnClickListener {
             mediaPlayer?.start()
@@ -49,8 +54,20 @@ class PlayBackActivity : AppCompatActivity() {
 
         stopButton.setOnClickListener {
             mediaPlayer?.stop()
-            mediaPlayer?.prepare() // Prepare media player for future play
+            mediaPlayer?.prepare() // Preparar para futuras reproducciones
         }
+    }
+
+    private fun setAudioImage(index: Int) {
+        val imageResId = when (index) {
+            0 -> R.drawable.queen_logo
+            1 -> R.drawable.guns_logo
+            2 -> R.drawable.radiohead_logo
+            3 -> R.drawable.thepolice_logo
+            4 -> R.drawable.losretros_logo
+            else -> R.drawable.music_icon // Imagen por defecto
+        }
+        audioImage.setImageResource(imageResId)
     }
 
     override fun onDestroy() {
