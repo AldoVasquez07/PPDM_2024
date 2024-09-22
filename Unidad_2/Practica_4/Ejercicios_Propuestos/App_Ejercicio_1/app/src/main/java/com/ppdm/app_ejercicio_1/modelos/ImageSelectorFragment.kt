ea5dffcc764cc4536b1231fd9667ae5a15185b8f
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-22 03:30:48 -0500

    Reutilizando la Clase Utils de la Práctica 3.

--- Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/modelos/ImageSelectorFragment.kt
+++ Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/modelos/ImageSelectorFragment.kt
@@ -1,6 +1,10 @@
 package com.ppdm.app_ejercicio_1.modelos
 
+import android.widget.Button
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 
 class ImageSelectorFragment: Fragment() {
+    private lateinit var imageSpinner: Spinner
+    private lateinit var nextButton: Button
 }
---
+++ Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/modelos/Utils.kt
@@ -0,0 +1,10 @@
+package com.ppdm.app_ejercicio_1.modelos
+
+object Utils {
+    // Lista de opciones de imagenes
+    val imageNames = arrayOf("Atardecer", "Nieve", "Campo")
+    // Valores constantes usados en los Activities
+    val spinnerPosition = "spinner_position"
+    val imageIndex = "image_index"
+    val savedImageIndex = "saved_image_index"
+}
