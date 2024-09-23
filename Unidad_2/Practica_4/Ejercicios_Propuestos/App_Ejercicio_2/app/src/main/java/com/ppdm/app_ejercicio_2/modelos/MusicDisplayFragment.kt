bf82b1279b76e3c2c8170623ca0ef0957e8934ce
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-23 09:46:45 -0500

    Creando Clases de Tipo Fragment: MusicDisplayFragment y MusicSelectorFragment

---
+++ Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_2/app/src/main/java/com/ppdm/app_ejercicio_2/modelos/MusicDisplayFragment.kt
@@ -0,0 +1,7 @@
+package com.ppdm.app_ejercicio_2.modelos
+
+import androidx.fragment.app.Fragment
+
+class MusicDisplayFragment: Fragment() {
+
+}
---
+++ Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_2/app/src/main/java/com/ppdm/app_ejercicio_2/modelos/MusicSelectorFragment.kt
@@ -0,0 +1,11 @@
+package com.ppdm.app_ejercicio_2.modelos
+
+import android.widget.Button
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+
+class MusicSelectorFragment: Fragment() {
+    private lateinit var musicSpinner: Spinner
+    private lateinit var nextButton: Button
+    
+}
