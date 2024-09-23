76b5ec2a3b542c324d03fa8a9f3fa7d1919caf79
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-09-22 21:43:50 -0500

    Agregando nav host a la clase MainActivity y al activity_main.xml, para la gestión de los fragments.

--- Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/controlador/MainActivity.kt
+++ Unidad_2/Practica_4/Ejercicios_Propuestos/App_Ejercicio_1/app/src/main/java/com/ppdm/app_ejercicio_1/controlador/MainActivity.kt
@@ -3,11 +3,17 @@ package com.ppdm.app_ejercicio_1.controlador
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ppdm.app_ejercicio_1.R
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Configurar NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
