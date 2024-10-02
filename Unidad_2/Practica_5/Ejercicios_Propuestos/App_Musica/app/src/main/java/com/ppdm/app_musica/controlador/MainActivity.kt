5643c1af655fd7c68ef78da02019139aedc8eaf0
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-01 22:03:06 -0500

    Estructurando contenido de los código como: Controlador, Modelos. Además, se ha creado la clase Utils.

--- Unidad_2/Practica_5/Ejercicios_Propuestos/App_Musica/app/src/main/java/com/ppdm/app_musica/controlador/MainActivity.kt
+++ Unidad_2/Practica_5/Ejercicios_Propuestos/App_Musica/app/src/main/java/com/ppdm/app_musica/controlador/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ppdm.app_musica
+package com.ppdm.app_musica.controlador
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ppdm.app_musica.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ Unidad_2/Practica_5/Ejercicios_Propuestos/App_Musica/app/src/main/java/com/ppdm/app_musica/modelos/Utils.kt
@@ -0,0 +1,6 @@
+package com.ppdm.app_musica.modelos
+
+// Clase Utils para el uso de aspectos regulares
+object Utils {
+    // Contenido Clase Utils
+}
